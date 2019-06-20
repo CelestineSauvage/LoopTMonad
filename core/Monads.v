80b8e14f20266e31bc26f82e0072ebc6edb2263f
celestine.sauvage <celestine.sauvage@gmail.com>
2019-06-20 16:20:26 +0200

    Proof of bind right unit for LoopeT monad

--- core/Monads.v
+++ core/Monads.v
@@ -62,6 +62,26 @@ Lemma bind_eq : forall {A B m} `{Monad m} (a a' : m A) (f f' : A -> m B),
 
 End monadic_functions.
 
+Ltac cbnify_monad_LHS :=
+  repeat match goal with
+  | [ |- bind (return_ _) _ = _ ] => rewrite <- bind_left_unit
+  | [ |- bind (bind _ _) _ = _ ]  => rewrite <- bind_associativity
+  | [ |- _ = _ ]                  => reflexivity
+  | [ |- bind ?a ?f = _ ]         => erewrite bind_eq; intros; 
+                                     [ | cbnify_monad_LHS | cbnify_monad_LHS ]
+  end.
+  
+Ltac cbnify_monad :=
+  cbnify_monad_LHS;
+  apply eq_sym;
+  cbnify_monad_LHS;
+  apply eq_sym.
+  
+Ltac cbn_m :=
+  repeat (try match goal with
+  [ |- bind ?a _ = bind ?a _ ] => apply bind_eq; [ reflexivity | intros ]
+  end; cbnify_monad).
+
 Section monadic_state.
 
 Variable S : Type.
@@ -151,7 +171,6 @@ Definition loopT_liftT {A} (x : m A) : LoopT e m A :=
 Global Program Instance LoopT_T  : MonadTrans (LoopT e):=
 { liftT := @loopT_liftT}.
   Next Obligation.
-  intros;cbn.
   unfold loopT_liftT.
   unfold loopT_pure.
   extensionality r.
@@ -191,6 +210,24 @@ Fixpoint foreach {m} `{Monad m}  (it min : nat) (body : nat -> LoopT unit m unit
       | 0 => return_ tt
     end.
 
+(* Program Fixpoint foreach3 (from to : Z) (body : Z -> LoopeT m unit) {measure (Z.abs_nat (to - from))} : m unit :=
+  if Z_lt_dec from to
+  then perf out <- runLoopeT (body from);
+                                match out with
+                                  | Break => return_ tt
+                                  | _ => foreach3 (from + 1) to body
+                                end
+  else return_ tt.
+Next Obligation. 
+apply Zabs_nat_lt; auto with zarith. Qed. *)
+
+(* Program Fixpoint foreach42 (from to : Z) (body : Z -> LoopT unit m unit) {measure (Z.abs_nat (to - from))} : m unit :=
+  if Z_lt_dec from to
+  then (stepLoopT (body (from + 1)) (fun _ => foreach42 (from + 1) to body))
+  else return_ tt.
+Next Obligation. 
+apply Zabs_nat_lt; auto with zarith. Qed. *)
+
 (* Fonction qui appelle une fois le corps de la boucle *)
 Definition once {m} `{Monad m} {a} (body : LoopT unit m a) : m unit :=
 stepLoopT body (fun _ => return_ tt).
@@ -199,41 +236,167 @@ End monadic_loop.
 
 Section monadic_loop2.
 
-Variable m : Type -> Type.
-Context `{Monad m}.
+Section monad_option.
 
 Inductive Action (A : Type) : Type :=
   | Break : Action A
+  | Continue : Action A
   | Atom : A -> Action A.
 
 Arguments Atom [A] _.
 Arguments Break [A].
+Arguments Continue [A].
 
-Definition LoopeT m a : Type := m (Action a).
+Global Program Instance actionM : Monad Action :=
+  { return_ := @Atom;
+    bind := fun A m B f => match m with Break => Break | Continue => Continue | Atom a => f a end
+  }.
+  Next Obligation.
+  destruct a ; auto.
+  Qed.
+  
+  Next Obligation.
+  destruct ma ; auto.
+  Qed.
 
-Definition runLoopeT {A} (loop : LoopeT m A) : m (Action A) :=
+Definition LoopeT m (a : Type) : Type := m (Action a).
+
+Definition loopeT_liftT {m A} `{Monad m} (a : m A) : LoopeT m A.
+Proof.
+unfold LoopeT.
+refine (perf x <- a; return_ (Atom x)).
+Defined.
+
+Definition runLoopeT {m A} (loop : LoopeT m A) : m (Action A) :=
   loop.
 
-Definition loopeT_pure {A} (a : A) : LoopeT m A :=
+Definition loopeT_pure {m} `{Monad m} {A} (a : A) : LoopeT m A :=
   return_ (Atom a).
+(*   @return_ m _ _ (return_ a). *)
 
-Definition loopeT_bind  {A} (x : LoopeT m A) {B} (k : A -> LoopeT m B) : LoopeT m B :=
+(* Definition loopeT_bind {m} `{Monad m} {A} (ma : LoopeT m A) {B} (f : A -> LoopeT m B)
+                        : LoopeT m B.
+Proof.
+unfold LoopeT in *.
+exact (perf oa <- ma; 
+         match oa with
+         | Break => return_ Break
+         | Continue => return_ Continue
+         | Atom a => f a
+         end
+  ).
+Defined. *)
+
+Definition loopeT_bind {m} `{Monad m} {A} (x : LoopeT m A) {B} (k : A -> LoopeT m B) : LoopeT m B :=
+    perf o <- runLoopeT x;
+    match o with
+      | Break => return_ Break
+      | Continue => return_ Continue
+      | Atom y => runLoopeT (k y)
+    end.
+
+Arguments Monad m : assert.
+
+(* Class Monad (m: Type -> Type) :=
+{ return_ : forall {A}, A -> m A;
+  bind: forall {A}, m A -> forall {B}, (A -> m B) -> m B;
+  bind_right_unit: forall A (a: m A), a = bind a return_;
+  bind_left_unit: forall A (a: A) B (f: A -> m B),
+             f a = bind (return_ a) f;
+  bind_associativity: forall A (ma: m A) B f C (g: B -> m C),
+                 bind ma (fun  x=> bind (f x) g) = bind (bind ma f) g
+}. *)
+
+Print bind.
+
+Close Scope monad_scope.
+
+Global Program Instance loopeT_M  {m} `{Monad m} : Monad (LoopeT m) :=
+  { return_ := @loopeT_pure m _;
+    bind := @loopeT_bind m _  }.
+  Next Obligation.
+  unfold loopeT_pure.
+  unfold loopeT_bind.
+  unfold runLoopeT.
+  unfold LoopeT in a.
+  generalize (bind_right_unit (Action A) a).
+  intros.
+  rewrite H0 at 1.
+  assert (forall o : Action A, match o with
+          | Break => return_ Break
+          | Continue => return_ Continue
+          | Atom y => return_ (Atom y)
+          end = return_ o).
+  intros.
+  case o ; auto.
+  apply f_equal.
+  symmetry.
+  apply functional_extensionality.
+  auto.
+  Qed.
+
+  Next Obligation.
+  unfold loopeT_pure.
+  unfold loopeT_bind.
+  unfold runLoopeT.
+  unfold LoopeT in a.
+  
+  rewrite <- H1.
+  assert ((fun o : Action A =>
+   match o with
+   | Break => return_ Break
+   | Continue => return_ Continue
+   | Atom y => return_ (Atom y)
+   end = return_)).
+  assert (bind a return_ = (@bind m H (Action A) a (Action A) return_)).
+  case_eq (@return_ (Action A)).
+  refine .
+
+
+(* Definition loopeT_bind  {A} (x : LoopeT m A) {B} (k : A -> LoopeT m B) : LoopeT m B :=
     perf o <- runLoopeT x;
     match o with
       | Break => return_ Break
       | Atom y => runLoopeT (k y)
-    end.
+    end. *)
 
-Definition loopeT_liftT {A} (a : m A) : LoopeT m A :=
-  liftM (@Atom A) a.
+(* Class Monad (m: Type -> Type) :=
+{ return_ : forall {A}, A -> m A;
+  bind: forall {A}, m A -> forall {B}, (A -> m B) -> m B;
+  bind_right_unit: forall A (a: m A), a = bind a return_;
+  bind_left_unit: forall A (a: A) B (f: A -> m B),
+             f a = bind (return_ a) f;
+  bind_associativity: forall A (ma: m A) B f C (g: B -> m C),
+                 bind ma (fun  x=> bind (f x) g) = bind (bind ma f) g
+}. *)
 
-Definition break {A} : LoopeT m A :=
+(* Notation "'perf' x '<-' m ';' e" := (bind m (fun x => e)) *)
+
+
+(*                         
+  Proof.
+  + intros.
+    unfold runLoopeT.
+    cbv.
+    case H.
+    intros.
+    generalize (bind_right_unit0 (Action A) a).
+    intros.
+    unfold LoopeT in *.
+    rewrite H0 at 1.
+    symmetry. *)
+    
+   
+(* Definition loopeT_liftT {m A} `{Monad m} (a : m A) : LoopeT m A :=
+  liftM (@Atom A) a. *)
+
+Definition break {m A} `{Monad m} : LoopeT m A :=
   return_ Break.
 
-Fixpoint foreach2 (it min : nat) (body : nat -> LoopeT m unit) : m unit :=
+Fixpoint foreach2 {m} `{Monad m}(it min : nat) (body : nat -> LoopeT m unit) : m unit :=
   if (it <=? min) then return_ tt
   else match it with
-        | S it' => perf out <- runLoopeT (body it');
+        | S it' => perf out <- (body it');
                                 match out with
                                   | Break => return_ tt
                                   | _ => foreach2 it' min body
@@ -270,4 +433,7 @@ Notation "'for2' i '=' max 'to' min '{{' body }}" := (foreach2 max min (fun i =>
 max at level 60, body at level 60, right associativity) : monad_scope.
 
 Notation "'for2_e' i '=' max 'to' min '{{' body }}" := (foreach2 min max (fun i => (body))) (at level 60, i ident, min at level 60,
+max at level 60, body at level 60, right associativity) : monad_scope.
+
+Notation "'for3' i '=' max 'to' min '{{' body }}" := (foreach2 min max (fun i => (body))) (at level 60, i ident, min at level 60,
 max at level 60, body at level 60, right associativity) : monad_scope.
